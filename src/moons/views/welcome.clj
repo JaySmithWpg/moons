4a472420fd035be312b82c6cf09eb3cda05f7618
Jay Smith <jayvsmith@gmail.com>
2013-01-01 12:23:48 -0600

    Updated gitignore

--- src/moons/views/welcome.clj
+++
@@ -1,9 +0,0 @@
-(ns moons.views.welcome
-  (:require [moons.views.common :as common]
-            [noir.content.getting-started])
-  (:use [noir.core :only [defpage]]))
-
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to moons"]))
-
